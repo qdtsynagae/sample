906d356e71c11a48027556838798c14775e6e3cc
qdtsynagae <qdts.y.nagae@gmail.com>
2026-01-09 13:43:43 +0900

    Create 4a26d9f427b0_baseline.py

---
+++ alembic/versions/4a26d9f427b0_baseline.py
@@ -0,0 +1,22 @@
+"""baseline
+
+Revision ID: 4a26d9f427b0
+Revises: 
+Create Date: 2026-01-09
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+revision = "4a26d9f427b0"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    # baseline placeholder
+    pass
+
+def downgrade() -> None:
+    pass
